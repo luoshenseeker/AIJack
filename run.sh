fab888e7408307ffbdf51dfccd28da0317ad9f8c
luoshenseeker <635598002@qq.com>
2022-05-23 14:40:09 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 # python src/gan_attack_implement.py -d --target_delta 1 --target_epsilon 1
 # python src/gan_attack_implement.py -d --target_delta 1 --target_epsilon 1
 # python src/gan_attack_implement.py
-python src/gan_attack_implement.py --dataset ATT -d --target_delta 1 --target_epsilon 200 -p
-python src/gan_attack_implement.py --dataset ATT -p
+# python src/gan_attack_implement.py --dataset ATT -d --target_delta 1 --target_epsilon 200 -p
+python src/gan_attack_implement.py --upload_p 0.5
+python src/gan_attack_implement.py --upload_p 0.1
+python src/gan_attack_implement.py --upload_p 0.05
+python src/gan_attack_implement.py --upload_p 0.01
